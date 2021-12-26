2013b9704e3e57ee84954cd50aaf75f05e61bce1
Feihong Hsu <feihong.hsu@gmail.com>
2021-12-26 08:35:55 -0600

    Add hello.py

---
+++ README.md
@@ -0,0 +1,20 @@
+# Feihong's PySide (Qt for Python) Quickstart
+
+## Prerequisites
+
+    curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+    # Update ~/.profile and ~/.bashrc according to instructions, then logout and login
+
+    pyenv install --list # list all versions you can install
+    pyenv install 3.10.1
+    pyenv global 3.10.1
+
+## Installation
+
+    pip install --user pyside6
+
+## Commands
+
+Run program
+
+    python hello.py
---
+++ hello.py
@@ -0,0 +1,38 @@
+"""
+Very basic GUI program. Show how to add shortcut and change font size of label.
+"""
+import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
+
+messages = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир", "你好世界"]
+
+class MyWidget(QtWidgets.QWidget):
+  def __init__(self):
+    super().__init__()
+
+    self.button = QtWidgets.QPushButton("Click me!")
+    self.label = QtWidgets.QLabel("Hello World",
+                                  alignment=QtCore.Qt.AlignCenter)
+    self.label.setFont(QtGui.QFont(self.font().family(), 40))
+
+    self.layout = QtWidgets.QVBoxLayout(self)
+    self.layout.addWidget(self.label)
+    self.layout.addWidget(self.button)
+
+    self.button.clicked.connect(self.on_click)
+
+    QtGui.QShortcut(QtGui.QKeySequence("Ctrl+Q"), self, self.close)
+
+  @QtCore.Slot()
+  def on_click(self):
+    self.label.setText(random.choice(messages))
+
+if __name__ == "__main__":
+  app = QtWidgets.QApplication([])
+
+  widget = MyWidget()
+  widget.resize(800, 600)
+  widget.show()
+
+  sys.exit(app.exec())
