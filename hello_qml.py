1dadd5e1c20e452a8f91441ac780e9722b6219b1
Feihong Hsu <feihong.hsu@gmail.com>
2022-01-01 14:57:45 -0600

    Quit if engine couldn't load QML file

--- hello_qml.py
+++ hello_qml.py
@@ -8,5 +8,7 @@ app = QGuiApplication(sys.argv)
 engine = QQmlApplicationEngine()
 engine.quit.connect(app.quit)
 engine.load('hello.qml')
+if not engine.rootObjects():
+  sys.exit(-1)
 
 sys.exit(app.exec())
